f425cd8865480805b3811737c62ca7ce5e5e634e
ErikaYsalazarV <Erika.Salazar@endava.com>
2023-03-14 19:31:28 -0500

    agrego la coleccion de CMC dentro de  db.py

--- src/db.py
+++ src/db.py
@@ -2,5 +2,6 @@ from flask_pymongo import pymongo
 
 CONNECTION_STRING = "mongodb+srv://carlosorduz:Endava2023@cluster0.njlnsoq.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('user_shopping_list')
+db = client.get_database('currency-monitoring')
+CMC_collection = pymongo.collection.Collection(db, 'coin_market_cap')
 user_collection = pymongo.collection.Collection(db, 'user_1_items')

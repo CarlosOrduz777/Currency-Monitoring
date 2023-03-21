959d58bc17575a07fe9717308c458d19c757680a
ErikaSalazar97 <eysalazarv@correo.udistrital.edu.co>
2023-03-21 09:48:24 -0500

    V3: agrega CORS para permitir solicitudes

--- src/app.py
+++ src/app.py
@@ -1,10 +1,8 @@
-from flask import Flask, Response, render_template 
+from flask import Flask, Response
+from flask_cors import CORS
 from flask_pymongo import pymongo
 from bson import json_util
-from werkzeug.local import LocalProxy
 from db import CMCHistorycollection
-import time
-import os
 from CMC import save_in_database
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -12,14 +10,21 @@ scheduler = BackgroundScheduler(daemon=True)
 scheduler.add_job(save_in_database, 'interval', seconds=300)
 scheduler.start()
 
-
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-@app.route('/v2/coins',methods=['GET'])
-def index():
+@app.route('/v3/coins', methods=['GET'])
+def get_coins():
     data_coins = CMCHistorycollection.find().sort('timestamp', pymongo.DESCENDING).limit(1)[0]
     response = json_util.dumps(data_coins)
-    return Response(response,mimetype='application/json')
+    return Response(response, mimetype='application/json')
 
-if __name__== "__main__":
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET')
+    return response
+
+if __name__ == "__main__":
     app.run(debug=True)
